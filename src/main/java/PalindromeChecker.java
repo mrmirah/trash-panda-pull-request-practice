01542a7a8e4b35f200880c4f1972067e84b0006d
mrmirah <33485576+mrmirah@users.noreply.github.com>
2024-11-15 08:35:53 -0500

    update

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -3,14 +3,14 @@ public class PalindromeChecker {
         String str = "madam";
         boolean isPalindrome = true;
 
-        for (int i = 0; i <= str.length() / 2; i++) {  // Logical error: <=
-            if (str.charAt(i) != str.charAt(str.length() - i)) {  // Index issue
+        for (int i = 0; i <= str.length() / 2; i++) {  
+            if (str.charAt(i) != str.charAt(str.length() - i)) { 
                 isPalindrome = false;
                 break;
             }
         }
 
-        if (isPalindrome = true) {  // Assignment instead of comparison
+        if (isPalindrome = true) {  
             System.out.println(str + " is a palindrome.");
         } else {
             System.out.println(str + " is not a palindrome.");
