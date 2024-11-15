5560d2538c05360494bbc1933ed84f6f992fd81b
anayak01 <anayak01@student.dasd.org>
2024-11-15 09:21:41 -0500

    Inside the second for loop I changed sum += number[i] / n by removing the / n

--- src/main/java/AverageCalculator.java
+++ src/main/java/AverageCalculator.java
@@ -16,7 +16,7 @@ public class AverageCalculator {
         int sum = 0;
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] == 0) continue;
-            sum += numbers[i] / n;
+            sum += numbers[i];
         }
 
         double average = (int) (sum / n);
